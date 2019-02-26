08d9a0bb123de7e1c812ff2025742fdeb12fe815
lianghuibiao <1767472324@qq.com>
2019-02-26 17:37:42 +0800

    Create card.html

--- fcc/find-the-longest-word-in-a-string.js
+++ fcc/find-the-longest-word-in-a-string.js
@@ -3,4 +3,4 @@ function findLongestWord(str) {
     return str.length;
   }
   
-  findLongestWord("The quick brown fox jumped over the lazy dog");
+  findLongestWord("The quick brown fox jumped over the lazy dog");    
--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
-console.log('hello');
+var a=0;
+var b=1;
+var c='';
+for(var i=0;i<=99;i++){
+    a=a+1;
+    c=c+a;
+                console.log(c+'\n');
+}
